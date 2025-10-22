45e091789c6e6a849a1bde201fdb233d84f80e9d
unknown <190011@bme.hu>
2025-10-22 17:51:42 +0200

    more coverage

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,50 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    void fire_MultipleTest() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(10);
+
+        // Act
+        boolean result = store.fire(4);
+
+        // Assert
+        assertEquals(true, result);
+        assertEquals(6, store.getTorpedoCount());
+    }
+
+    @Test
+    void fire_FailureRateTest() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1, 1.0);
+
+        // Act
+        boolean result = store.fire(1);
+
+        // Assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    void fire_NotEnoughTopedosTest() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(0);
+
+        // Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> store.fire(1));
+    }
+
+    @Test
+    void isEmptyTest() {
+        // Arrange
+        TorpedoStore store1 = new TorpedoStore(1);
+        TorpedoStore store2 = new TorpedoStore(0);
+
+        // Assert
+        assertEquals(false, store1.isEmpty());
+        assertEquals(true, store2.isEmpty());
     }
 }
